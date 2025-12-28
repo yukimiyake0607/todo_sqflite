11ca0d983b37611a67e0fcabdd6b5d72bafb9a8d
三宅柚希 <yukimiyake38@gmail.com>
2025-12-28 21:12:50 +0900

    add mypage_screen

---
+++ packages/todo_sqflite/lib/src/feature/mypage/mypage_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MypageScreen extends StatelessWidget {
+  const MypageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Mypage')),
+      body: Center(child: Text('Mypage')),
+    );
+  }
+}
