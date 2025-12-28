1672624f86d81a974e025e0b963780909cb814c8
三宅柚希 <yukimiyake38@gmail.com>
2025-12-28 21:12:43 +0900

    add todo_screen

---
+++ packages/todo_sqflite/lib/src/feature/todo/todo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatelessWidget {
+  const TodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List')),
+      body: Center(child: Text('Todo List')),
+    );
+  }
+}
